7e6e12b10fdb0e5aaa9e688870933724f275275b
Pharaon3 <Pharaon313@outlook.com>
2024-03-25 18:03:34 +0900

    .gitignore changed

---
+++ application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'R4XNCS-UDW46S-V3VVBJ-RFZLOXhMdllPRERRSFhmK3k5K01heW4wbWo2aU1oWXYxTk4rVjBtY3dMQT0=';
+$config['app_ver'] = 0;
